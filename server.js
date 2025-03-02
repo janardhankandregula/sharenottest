a80595059236eb9673fc96ad3de85906ba9c2caf
janardhankandregula <janardhank134@gmail.com>
2025-03-01 22:54:46 -0500

    Your commit message454454

--- server.js
+++ server.js
@@ -72,5 +72,5 @@ app.post("/api/notes", async (req, res) => {
 //module.exports = app;
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
